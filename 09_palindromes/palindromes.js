34cc41a0a50f972c8c717c1dd4a9f6f6d67ef4b5
Ray <kavlekar101@gmail.com>
2021-12-28 18:07:07 -0500

    read the comments in the code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+/*
+\w is any digit, letter, or underscore.
+\s is any whitespace.
+[^\w\s] is anything that's not a digit, letter, whitespace, or underscore.
+[^\w\s]|_ is the same as #3 except with the underscores added back in.
+*/
+const palindromes = function (str) {
+    newStr = str.replace(/[^\w\s]|_/g, "")
+        .replace(/\s+/g, "");
+    let length = newStr.length;
+    let half = length / 2;
+    let firstHalf = newStr.substr(0, half).toLowerCase();
+    let secondHalf = newStr.substr(half * -1).toLowerCase();
+    return firstHalf.localeCompare(secondHalf.split("").reverse().join("")) == 0;
 };
 
 // Do not edit below this line
