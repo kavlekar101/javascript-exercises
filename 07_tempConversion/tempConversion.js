23f5621cc274c7ef526e41143583705d18dd7fc7
Ray <kavlekar101@gmail.com>
2021-12-28 17:52:30 -0500

    check to make sure the types are the same before comparing for equality

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let conversion = (num - 32) * (5 / 9);
+  if(!Number.isInteger(conversion)){
+    return parseFloat(conversion.toFixed(1));
+  }
+  return conversion;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let conversion = num * (9 / 5) + 32;
+  if (!Number.isInteger(conversion)) {
+    return parseFloat(conversion.toFixed(1));
+  }
+  return conversion;
 };
 
 // Do not edit below this line
