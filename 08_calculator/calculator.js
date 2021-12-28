6db0a548f6adf7eb43a334e85ffaab7d74a8812f
Ray <kavlekar101@gmail.com>
2021-12-28 17:58:07 -0500

    for each loop is aliasing I believe, so adding the items in the for each doesn't work

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum1 = 0;
+	for(let i = 0; i < arr.length; i++){
+    sum1 += arr[i];
+  }
+  return sum1;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let product1 = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product1 *= arr[i];
+  }
+  return product1;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  for(let i = num; i > 0; i--){
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
