29930e6c433881437c77e3253c89cad1597f440b
Ray <kavlekar101@gmail.com>
2021-12-28 17:19:53 -0500

    There's a repeat method for strings, but doesn't take negative values

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0) {
+        return 'ERROR';
+    }
+    let strFinal = str.repeat(num);
+    return strFinal;
 };
 
 // Do not edit below this line
