93aeef7b6adfe042dd1df01ec25c3312ba83f3b7
Ray <kavlekar101@gmail.com>
2021-12-28 17:30:27 -0500

    Run npm test <filename>

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let argsLength = arguments.length;
+    for(let i = 1; i < argsLength; i++) {
+        let index = arr.indexOf(arguments[i]);
+        if (index != -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 
 };
 
