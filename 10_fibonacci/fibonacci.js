018d74024db0279301cb2e62fc9499d14d55ab0c
Ray <kavlekar101@gmail.com>
2021-12-29 08:17:58 -0500

    == and === are different, but recursion works the same

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num == 1 || num == 2) {
+        return 1;
+    } else if (num == 0){
+        return 0;
+    } else {
+        if(num < 0) {
+            return 'OOPS';
+        }
+        let fib = fibonacci(num - 1) + fibonacci(num - 2);;
+        /*if (num instanceof String) {
+            fib = fibonacci(num.valueOf());
+        } else {
+            fib = fibonacci(num - 1) + fibonacci(num - 2);
+        }*/
+        // don't need commented code because == doesn't check for type equality, 
+        // only the values of the objects reardless of typing
+        return fib;
+    }
 };
 
 // Do not edit below this line
