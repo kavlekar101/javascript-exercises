fdecea8a480414fca199b3c79f1e8143f2f7c2f8
Ray <kavlekar101@gmail.com>
2021-12-29 11:18:20 -0500

    ASCII values are the same, but there are specific functions that are more helpful in javascript than java

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,34 @@
-const caesar = function() {
-
+function newChar(str, i, moveBy, offset) {
+    let newCode = str.charCodeAt(i) + moveBy;
+    if (newCode - offset > 25) {
+        newCode = newCode - offset - 26;
+        while(newCode >= 26) {
+            newCode -= 26;
+        }
+        newCode += offset;
+    } else if (newCode - offset < 0) {
+        newCode = newCode - offset;
+        while (newCode < 0) {
+            newCode += 26;
+        }
+        newCode += offset;
+    }
+    return String.fromCharCode(newCode);
+}
+const caesar = function(str, moveBy) {
+    let newStr = "";
+    for(let i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) - 65 < 26 && str.charCodeAt(i) - 65 >= 0){
+            // we know that it is uppercase
+            newStr += newChar(str, i, moveBy, 65);
+        } else if (str.charCodeAt(i) - 97 < 26 && str.charCodeAt(i) - 97 >= 0){
+            // we know that it is lowercase
+            newStr += newChar(str, i, moveBy, 97);
+        } else {
+            newStr += str.charAt(i);
+        }
+    }
+    return newStr;
 };
 
 // Do not edit below this line
